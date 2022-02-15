32fe328fddd030883f5cde80dbd9525ad7205213
IrfanAhmadAsqolani <imadas19@gmail.com>
2022-02-15 14:51:32 +0700

    Penambahan info kelas

--- Tugas_Alterra.py
+++ Tugas_Alterra.py
@@ -2,4 +2,4 @@ print("Selamat Datang di Website Pembelajaran Alttera Academy")
 print("Silahkan isi biodata peserta kampus merdeka terlebih dahulu")
 print("Nama : Irfan Ahmad Asqolani")
 print("Domisili : Tangerang")
-
+print("Kelas : How to be an Ideal Top Search React JS Front-End Engineer")
