013321f32bd28f216cec7cf4c89fc829ed2d8ca2
IrfanAhmadAsqolani <imadas19@gmail.com>
2022-02-15 14:54:13 +0700

    Penambahan info jenis kelamin

--- Tugas_Alterra.py
+++ Tugas_Alterra.py
@@ -2,4 +2,4 @@ print("Selamat Datang di Website Pembelajaran Alttera Academy")
 print("Silahkan isi biodata peserta kampus merdeka terlebih dahulu")
 print("Nama : Irfan Ahmad Asqolani")
 print("Domisili : Tangerang")
-
+print("Jenis Kelamin : Laki-Laki")
