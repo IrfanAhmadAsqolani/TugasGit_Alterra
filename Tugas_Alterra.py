5d61b075e84f2060c42ad1ae1a46cde7a88b4faf
IrfanAhmadAsqolani <imadas19@gmail.com>
2022-02-15 14:48:02 +0700

    Inisialisasi Project praktikum alterra

---
+++ Tugas_Alterra.py
@@ -0,0 +1,5 @@
+print("Selamat Datang di Website Pembelajaran Alttera Academy")
+print("Silahkan isi biodata peserta kampus merdeka terlebih dahulu")
+print("Nama : Irfan Ahmad Asqolani")
+print("Domisili : Tangerang")
+
